4b894831f0c2f3d8ef904a275096e844c2905259
Kenneth Martinez <kenneth@Kenneths-MacBook-Pro.local>
2021-03-08 16:14:58 -0500

    2nd revision to async

--- 2.1_asyncAwait.instructional.js
+++ 2.1_asyncAwait.instructional.js
@@ -8,36 +8,21 @@
 //NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
 
    
-     fetch("https://swapi.dev/api/people/3/")
-    .then((res) => {
-        return res.json()
-    })
+    async function char (){
+    let swapi = await fetch("https://swapi.dev/api/people/3/")
+    let second = await function jsonified () {
+        ((swapi) => {
+           return swapi.json
+        })
+    }
+    console.log(swapi);
+    console.log(second);
+     console.log("this should print last");
 
-    ,then(json => {
-        console.log(json)
-    })
-    // console.log(res);
-
-// fetch('https://rickandmortyapi.com/api/character')
-// .then ((res) => {
-//     return res.json()
-// })
-
-// .then(json => {
-//     console.log(json);
-//     rickAndMorty(json);
-// } ) 
- 
-/*
-function rickAndMorty(character){
-        let rick = character.results[0].image
-        let morty = character.results[1].image
-        image1.src= rick
-        image2.src= morty
-        console.log(rick)
-        console.log(morty)
-        
     }
 
+    char();
 
-*/
+  
+
+    
